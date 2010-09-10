7689e6e7b49758d60ee42cfadfced9d13f6242b8
Boyd Stephen Smith Jr <bss@iguanasuicide.net>
2010-09-10 13:10:19 -0500

    Tweak to not "abandon" planets and consider growth on enemy planets.

--- Main.hs
+++ Main.hs
@@ -25,26 +25,31 @@ doTurn state = if null myPlanets || null targets
     -- Longest trip from one of my planets
     maxDistance p = maximum $ map (distanceBetween p) myPlanets
 
-    -- Ships available to send
-    availableShips = sum $ map planetShips myPlanets
+    -- Ships available to send (~60%)
+    availableShips = (total * 6) `div` 10
+      where total = sum $ map planetShips myPlanets
+
+    extendPlanet p = (p, (distance, ships))
+      where
+        distance = ceiling $ maxDistance p
+        ships = if isHostile p then planetGrowthRate p * distance else 0
+              + planetShips p
 
     -- Heuristic value of a planet.
     -- Planets with high production are preferred.
     -- In case of tie, use the closet one.
     -- In case of tie, use the one that takes the fewest ships to conquer
-    value p = (-growth, distance, ships)
+    value (p, (d, s)) = (-growth, d, s)
       where
         growth = planetGrowthRate p
-        distance = maxDistance p
-        ships = planetShips p
 
     -- Target the "best" planet that we have enough ships to take.
-    targets = filter ((< availableShips) . planetShips)
-           $ sortBy (comparing value) notMyPlanets
+    targets = filter ((< availableShips) . snd . snd)
+           $ sortBy (comparing value) $ map extendPlanet notMyPlanets
     target = head targets
 
     -- Send at least enough ships to conquer it.
-    totalFleetSize = planetShips target + 1
+    totalFleetSize = (snd $ snd target) + 1
 
     -- Per-planet fleet size
     fleetSize p = (totalFleetSize * planetShips p) `divCeil` availableShips
@@ -53,7 +58,7 @@ doTurn state = if null myPlanets || null targets
           where (q, r) = x `divMod` y
 
     -- Per-planet orders
-    order p = Order (planetId p) (planetId target) (fleetSize p)
+    order p = Order (planetId p) (planetId $ fst target) (fleetSize p)
 
     -- All orders
     orders = filter ((> 0) . orderShips) $ map order myPlanets
