eedc398bda502a0cb2f3268ec58d9e4a7e13431c
Boyd Stephen Smith Jr <bss@iguanasuicide.net>
2010-09-10 12:27:56 -0500

    Custom strategy

--- Main.hs
+++ Main.hs
@@ -2,7 +2,7 @@
 --
 module Main where
 
-import Data.List (maximumBy, minimumBy, partition)
+import Data.List (maximumBy, minimumBy, partition, sortBy)
 import qualified Data.IntMap as IM
 import Data.Ord (comparing)
 
@@ -10,11 +10,11 @@ import PlanetWars
 
 doTurn :: GameState  -- ^ Game state
        -> [Order]    -- ^ Orders
-doTurn state = if null myFleets
-    -- Simple ai
-    then [Order (planetId strongest) (planetId weakest) ships]
+doTurn state = if null myPlanets || null targets
+    -- No valid orders
+    then []
     -- If we have a fleet in flight, just do nothing
-    else []
+    else orders
   where
     myFleets = filter isAllied $ gameStateFleets state
 
@@ -22,12 +22,41 @@ doTurn state = if null myFleets
     (myPlanets, notMyPlanets) = partition isAllied $
         map snd $ IM.toList $ gameStatePlanets state
 
-    -- Find our strongest planet and the weakest neutral/hostile planet
-    strongest = maximumBy (comparing planetShips) myPlanets
-    weakest = minimumBy (comparing planetShips) notMyPlanets
+    -- Longest trip from one of my planets
+    maxDistance p = maximum $ map (distanceBetween p) myPlanets
 
-    -- Select half of the ships
-    ships = planetShips strongest `div` 2
+    -- Ships available to send
+    availableShips = sum $ map planetShips myPlanets
+
+    -- Heuristic value of a planet.
+    -- Planets with high production are preferred.
+    -- In case of tie, use the closet one.
+    -- In case of tie, use the one that takes the fewest ships to conquer
+    value p = (-growth, distance, ships)
+      where
+        growth = planetGrowthRate p
+        distance = maxDistance p
+        ships = planetShips p
+
+    -- Target the "best" planet that we have enough ships to take.
+    targets = filter ((< availableShips) . planetShips)
+           $ sortBy (comparing value) notMyPlanets
+    target = head targets
+
+    -- Send at least enough ships to conquer it.
+    totalFleetSize = planetShips target + 1
+
+    -- Per-planet fleet size
+    fleetSize p = (totalFleetSize * planetShips p) `divCeil` availableShips
+      where
+        divCeil x y = if r > 0 then q + 1 else q
+          where (q, r) = x `divMod` y
+
+    -- Per-planet orders
+    order p = Order (planetId p) (planetId target) (fleetSize p)
+
+    -- All orders
+    orders = filter ((> 0) . orderShips) $ map order myPlanets
 
 main :: IO ()
 main = bot doTurn
