8b13794ccd0861d9edb89ea39ce970a4e1a72e68
Boyd Stephen Smith Jr <bss@iguanasuicide.net>
2010-11-23 23:15:58 -0600

    Break out of test loops early if java fails.

--- test.sh
+++ test.sh
@@ -6,12 +6,17 @@ for bot in Random Bully Prospector Dual Rage; do
 	for map in maps/map*.txt; do
 		java -jar tools/PlayGame.jar "$map" 1000 200 PlayGame.log \
 			"$prog" "$botbin" > last.out 2> last.err
+		err=$?
 		echo "$map" > last.map
 		grep -v '^Turn' last.err
 		grep -q 'timed out' last.err && break
+		[ "$err" -eq 0 ] || break
 	done > results
 	printf 'Against %sBot\n' "$bot"
 	sort results | uniq -c
 	grep -q 'timed out' results && break
+	[ "$err" -eq 0 ] || break
 done
+[ "$err" -eq 0 ] || exit $err
+
 ! grep -q 'timed out' results
